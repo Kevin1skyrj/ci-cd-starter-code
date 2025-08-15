ce154946ebbb6f0116239413792dcb71105259e5
Rajat Pandey <rajatpndey257@gmail.com>
2025-08-15 11:00:17 +0530

    ci/cd using Github action

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-2xl font-bold justify-center items-center'>Welcome to Github Actions</div>
+  )
+}
+
+export default App
